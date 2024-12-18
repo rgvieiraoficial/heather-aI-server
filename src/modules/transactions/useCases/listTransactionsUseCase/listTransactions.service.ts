fbe35da6255343f534d5f0af7cff576a167cf345
Yami Renato <yami.hiei87@gmail.com>
2024-12-18 01:57:26 -0300

    Added List Transactions Controller and Services

---
+++ src/modules/transactions/useCases/listTransactionsUseCase/listTransactions.service.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Transactions } from '@prisma/client';
+
+import { ITransactionsRepository } from '../../repositores/ITransactionsRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+@Injectable()
+export class ListTransactionsService {
+  constructor(
+    @Inject('TransactionsRepository')
+    private readonly transactionsRepository: ITransactionsRepository
+  ) { }
+
+  async execute({ user_id }: IRequest): Promise<Transactions[]> {
+    const response = await this.transactionsRepository.list(user_id);
+
+    return response;
+  }
+}
