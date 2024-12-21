d69505fee44d556e15fdb2b3d24e293edba2fea6
Yami Renato <yami.hiei87@gmail.com>
2024-12-21 18:46:18 -0300

    Added Cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = Number(process.env.PORT) || 3334;
   await app.listen(port);
   console.log(`HTTP server listening on port ${port}`);
