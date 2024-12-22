c0f856e9aa12848a1a7c92ac7ed4d37e179deba0
Yami Renato <yami.hiei87@gmail.com>
2024-12-22 10:08:51 -0300

    Added Create Message Socket Service Integration

--- src/websockets/websockets.module.ts
+++ src/websockets/websockets.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaModule } from '../lib/prisma/prisma.module';
+
 import { ChatGateway } from './gateways/chat.gateway';
 
+import { ChatsRepository } from '../modules/chats/repositories/implementations/chats.repository';
+import { MessagesRepository } from '../modules/messages/repositories/implementations/messages.repository';
+import { CreateMessageService } from '../modules/messages/useCases/createMessageUseCase/createMessage.service';
+
 @Module({
-  providers: [ChatGateway]
+  imports: [PrismaModule],
+  providers: [
+    ChatGateway,
+    CreateMessageService,
+    {
+      provide: 'ChatsRepository',
+      useClass: ChatsRepository,
+    },
+    {
+      provide: 'MessagesRepository',
+      useClass: MessagesRepository,
+    }
+  ]
 })
 export class WebsocketsModule { }
